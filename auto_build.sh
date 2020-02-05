ecc8fe9760d9ea4b209685046c467af0501a16aa
Kevin Jen <kevin7674@gmail.com>
2020-02-05 16:43:07 +0800

    Update auto_build.sh

--- auto_build.sh
+++ auto_build.sh
@@ -5,7 +5,6 @@ containername="airflow"
 docker stop $containername
 docker rm $containername
 docker build -t $imagename .
-docker run --runtime=nvidia \
-	-ti -d -p 30001:8080 \
+docker run -ti -d -p 30001:8080 \
 	--name $containername \
 	$imagename /bin/bash
